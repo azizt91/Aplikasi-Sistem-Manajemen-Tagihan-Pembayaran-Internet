6fa3d10543f9f1e5d7a42611265d8df51741ab17
azizt91 <128366806+azizt91@users.noreply.github.com>
2025-05-01 11:18:15 +0700

    Update footer.blade.php

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -6,8 +6,11 @@
         <script>
           document.write(new Date().getFullYear());
         </script>
-        , made with ❤️ by
-        <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+        | Hak Cipta | Follow
+        <a href="https://www.instagram.com/azizt91" target="_blank" class="footer-link fw-bolder">
+            <img src="/sneat/assets/img/icons/brands/instagram.png" alt="Instagram" style="height: 20px; width: 20px;">
+            @azizt91
+        </a>
       </div>
       </div>
     </div>
