185a7d61d8dc0af75e52852b980e03cd0e4387d6
azizt91 <128366806+azizt91@users.noreply.github.com>
2025-05-13 16:16:42 +0700

    Update SendFonnteNotifications.php

--- app/Console/Commands/SendFonnteNotifications.php
+++ app/Console/Commands/SendFonnteNotifications.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Http\Controllers\FonnteController;
+
+
+class SendFonnteNotifications extends Command
+{
+    protected $signature = 'fonnte:send-notifications';
+    protected $description = 'Mengirim pesan otomatis ke pelanggan melalui Fonnte';
+
+    public function handle()
+    {
+        $controller = new FonnteController();
+        $controller->sendNotifications();
+
+        $this->info('Pesan otomatis telah dikirim!');
+    }
+}
