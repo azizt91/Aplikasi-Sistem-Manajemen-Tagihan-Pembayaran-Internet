61197d9900b7c73bbd0202bcc3e4ff74d40a6b01
azizt91 <128366806+azizt91@users.noreply.github.com>
2025-06-19 18:34:12 +0700

    Update footer.blade.php

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,12 +1,5 @@
  <!-- Footer -->
 
- <!-- Donate Button (Floating at Bottom Right) -->
-<div class="donate-button">
-    <a class="nav-link" href="https://saweria.co/azizt91" target="_blank">
-        <img src="/sneat/assets/img/icons/brands/donate.png" alt="Saweria" class="img-fluid">
-    </a>
-</div>
-
  <footer class="content-footer footer bg-footer-theme">
     <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
       <div class="mb-2 mb-md-0">
@@ -14,11 +7,7 @@
         <script>
           document.write(new Date().getFullYear());
         </script>
-        | Hak Cipta | Follow
-        <a href="https://www.instagram.com/azizt91" target="_blank" class="footer-link fw-bolder">
-            <img src="/sneat/assets/img/icons/brands/instagram.png" alt="Instagram" style="height: 20px; width: 20px;">
-            @azizt91
-        </a>
+        | Selinggonet
       </div>
       </div>
     </div>
